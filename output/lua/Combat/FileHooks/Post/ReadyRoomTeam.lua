8d121752dfdc9f65188bfb4b1220e4a17f6daf70
Sebastian Schuck <sebastian@naturalselection2.com>
2018-05-19 11:44:05 +0200

    Fixed some script errors that occurred at co_portals

--- output/lua/Combat/FileHooks/Post/ReadyRoomTeam.lua
+++ output/lua/Combat/FileHooks/Post/ReadyRoomTeam.lua
@@ -1,26 +1,10 @@
-function ReadyRoomTeam:GetRespawnMapName(player)
+-- Fixes for co_portals
+-- Todo: Merge into vanilla
+function ReadyRoomTeam:OnConstructionComplete()
+end
 
-    local mapName = player.kMapName    
-    
-    if mapName == nil then
-        mapName = ReadyRoomPlayer.kMapName
-    end
-    
-    -- Use previous life form if dead or in commander chair
-    if (mapName == MarineCommander.kMapName) 
-       or (mapName == AlienCommander.kMapName) 
-       or (mapName == Spectator.kMapName) 
-       or (mapName == AlienSpectator.kMapName) 
-       or (mapName ==  MarineSpectator.kMapName) then 
-    
-        mapName = player:GetPreviousMapName()
-        
-    end
-    
-    -- need to set embryos to ready room players, otherwise they wont be able to move
-    if mapName == Embryo.kMapName then
-        mapName = ReadyRoomPlayer.kMapName
-    end
-    return mapName
-    
+function ReadyRoomTeam:OnUpgradeChamberConstructed()
+end
+
+function ReadyRoomTeam:OnUpgradeChamberDestroyed()
 end
