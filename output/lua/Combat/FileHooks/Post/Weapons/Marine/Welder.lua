03d96aafe8bc0acea1161384b8fa118e758c10c9
Nin <therealnin@users.noreply.github.com>
2018-05-26 20:00:04 -0400

    Add self-weld to the welder now that we have focus

---
+++ output/lua/Combat/FileHooks/Post/Weapons/Marine/Welder.lua
@@ -0,0 +1,55 @@
+
+function Welder:PerformWeld(player)
+
+    local attackDirection = player:GetViewCoords().zAxis
+    local success = false
+    -- prioritize friendlies
+    local didHit, target, endPoint, direction, surface = CheckMeleeCapsule(self, player, 0, self:GetRange(), nil, true, 1, PrioritizeDamagedFriends, nil, PhysicsMask.Flame)
+    
+    if didHit and target and HasMixin(target, "Live") then
+        
+        if GetAreEnemies(player, target) then
+            self:DoDamage(kWelderDamagePerSecond * kWelderFireDelay, target, endPoint, attackDirection)
+            success = true     
+        elseif player:GetTeamNumber() == target:GetTeamNumber() and HasMixin(target, "Weldable") then
+        
+            if target:GetHealthScalar() < 1 then
+                
+                local prevHealthScalar = target:GetHealthScalar()
+                local prevHealth = target:GetHealth()
+                local prevArmor = target:GetArmor()
+                target:OnWeld(self, kWelderFireDelay, player)
+                success = prevHealthScalar ~= target:GetHealthScalar()
+                
+                if success then
+                
+                    local addAmount = (target:GetHealth() - prevHealth) + (target:GetArmor() - prevArmor)
+                    player:AddContinuousScore("WeldHealth", addAmount, Welder.kAmountHealedForPoints, Welder.kHealScoreAdded)
+                    
+                    -- weld owner as well
+                    player:SetArmor(player:GetArmor() + kWelderFireDelay * kSelfWeldAmount)
+                    
+                end
+                
+            end
+            
+            if HasMixin(target, "Construct") and target:GetCanConstruct(player) then
+                target:Construct(kWelderFireDelay, player)
+            end
+            
+        end
+    else
+        local dot = Math.DotProduct(attackDirection, Vector(0,-1,0))
+        if dot > 0.9 then
+        
+            -- weld owner
+            player:SetArmor(player:GetArmor() + kWelderFireDelay * kSelfWeldAmount)
+            
+        end
+    end
+    
+    if success then    
+        return endPoint
+    end
+    
+end
