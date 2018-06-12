e65b91072fa992dca66481f481b4da880214fdc6
Nin <therealnin@users.noreply.github.com>
2018-06-11 21:14:49 -0400

    Fix Shine's alltalk text

--- output/lua/Combat/Client.lua
+++ output/lua/Combat/Client.lua
@@ -7,3 +7,12 @@ do
     AddClientUIScriptForTeam(kTeam2Index, "Combat/GUIExperienceBar")
     AddClientUIScriptForTeam(kTeam2Index, "Combat/GUIGameTimeCountDown")
 end
+
+
+
+if Shine and Shine.Hook and Shine.Hook.Add then
+    Shine.Hook.Add( "OnUpdateAllTalkText", "combatFunction", function( OldFunc, ... )
+        -- Move the text to the left of the bottom of the screen
+        return true, 0.25, 0.95
+    end )
+end
