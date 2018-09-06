2820ee95fa447ab0c99d0b750cf6753e803aef7a
Nin <therealnin@users.noreply.github.com>
2018-09-06 16:18:47 -0400

    Fix weird hydra animation

--- output/lua/Combat/FileHooks/Post/Hydra.lua
+++ output/lua/Combat/FileHooks/Post/Hydra.lua
@@ -1,27 +1,31 @@
--- Implement lvl and XP
-local oldOnUpdate = Hydra.OnUpdate
-function Hydra:OnUpdate(...)
-    oldOnUpdate(self, ...)
 
-    -- check if the owner is still a gorge
-    if self.hydraParentId then
-        local owner = Shared.GetEntity(self.hydraParentId)
-        if owner then
-            if not owner:isa("Gorge") then
-                -- start a timer, if the player is still no gorge when the timer is 0, kill the hydras
-                if not self.killTime then
-                    self.killTime = Shared.GetTime() + kHydraKillTime
-                end
-                
-                if Shared.GetTime() >= self.killTime then
-                    self:Kill()
-                end
-                
-            else
-                self.killTime = nil
-            end 
-        else
-            self:Kill()
-        end   
-    end
+
+-- kill hydra if owner is no longer a gorge
+if Server then
+	local oldOnUpdate = Hydra.OnUpdate
+	function Hydra:OnUpdate(...)
+		oldOnUpdate(self, ...)
+
+		-- check if the owner is still a gorge
+		if self.hydraParentId then
+			local owner = Shared.GetEntity(self.hydraParentId)
+			if owner then
+				if not owner:isa("Gorge") then
+					-- start a timer, if the player is still no gorge when the timer is 0, kill the hydras
+					if not self.killTime then
+						self.killTime = Shared.GetTime() + kHydraKillTime
+					end
+					
+					if Shared.GetTime() >= self.killTime then
+						self:Kill()
+					end
+					
+				else
+					self.killTime = nil
+				end 
+			else
+				self:Kill()
+			end   
+		end
+	end
 end
