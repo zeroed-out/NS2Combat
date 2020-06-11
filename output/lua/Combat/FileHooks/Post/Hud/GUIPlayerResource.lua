3415d6e69892b417c2bf4a23056aad1523c9204d
Sebastian Schuck <sebastian@naturalselection2.com>
2020-06-11 20:16:37 +0200

    Build 332 compatibility
    Also block new top bar as it overlaps woth combats one

--- output/lua/Combat/FileHooks/Post/Hud/GUIPlayerResource.lua
+++ output/lua/Combat/FileHooks/Post/Hud/GUIPlayerResource.lua
@@ -1,13 +1,3 @@
--- Hide the TEAM RES
-local oldUpdate = GUIPlayerResource.Update
-function GUIPlayerResource:Update(...)
-
-    oldUpdate(self, ...)
-
-	self.teamText:SetText("")
-
-end
-
 -- Upgrade Points instead of RESOURCES
 local oldInitialize = GUIPlayerResource.Initialize
 function GUIPlayerResource:Initialize(...)
