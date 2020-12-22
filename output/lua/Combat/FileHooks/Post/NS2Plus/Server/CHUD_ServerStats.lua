b3cb16c6de45b330928f9916d986d562a4686895
diego <1570625+ailmanki@users.noreply.github.com>
2020-12-22 23:23:08 +0100

    expireTime found

--- output/lua/Combat/FileHooks/Post/NS2Plus/Server/CHUD_ServerStats.lua
+++
@@ -1,8 +0,0 @@
-
-local originalMedPackOnTouch
-originalMedPackOnTouch = Class_ReplaceMethod("MedPack", "OnTouch",
-	function(self, recipient)
-        self.expireTime = 0
-		originalMedPackOnTouch(self, recipient)
-	end)
-	
