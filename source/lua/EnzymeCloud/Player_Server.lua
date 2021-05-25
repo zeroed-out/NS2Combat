ba6aefb4534ca5bdee15f1cab67fa5fcbbbac2b5
zeroed-out <zeroed.out@nonmail.com>
2021-05-24 23:55:14 -0400

    Fixed timing errors with Enzyme, no longer spammable

--- source/lua/EnzymeCloud/Player_Server.lua
+++ source/lua/EnzymeCloud/Player_Server.lua
@@ -9,18 +9,17 @@ function Player:TriggerAlert(techId, entity, force)
     if techId ~= kTechId.AlienAlertNeedDrifter or not entity or not entity.combatTable or not entity.combatTable.hasEnzymeCloud then
         return
     end
-    local canEnzyme = entity.combatTable.lastEnzymeCloud <= Shared.GetTime()
+    local canEnzyme = entity.combatTable.lastEnzymeCloud + kEnzymeCloudAbilityCooldown <= Shared.GetTime()
 
 	if entity:isa("Player") and entity:GetIsAlive() and canEnzyme then
 		
-		local newTime = Shared.GetTime() + kEnzymeCloudAbilityCooldown
-		entity.combatTable.lastEnzymeCloud = newTime
+		entity.combatTable.lastEnzymeCloud = Shared.GetTime()
 		local position = entity:GetOrigin()
         local enzyme = CreateEntity(EnzymeCloud.kMapName, position , self:GetTeamNumber())
         enzyme:TriggerEffects( "drifter_shoot_enzyme", { effecthostcoords = Coords.GetTranslation(position) } )
 
     elseif not canEnzyme then
-        local timeReady = math.abs(entity.combatTable.lastEnzymeCloud + kEnzymeCloudAbilityCooldown - Shared.GetTime())
+        local timeReady = math.abs(math.ceil(entity.combatTable.lastEnzymeCloud + kEnzymeCloudAbilityCooldown - Shared.GetTime()))
         entity:SendDirectMessage("Enzyme ready in " .. timeReady .. " sec")
     end		
 	
